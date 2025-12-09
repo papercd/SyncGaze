d31c075ceaa9f957cf1294b10d2e9731c5da7430
JuhwanLee99 <jimmy3470second@gmail.com>
2025-12-09 19:46:36 +0900

    feat: cypress 테스트 구축

--- frontend/cypress.config.cjs
+++ frontend/cypress.config.cjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'cypress';
+const { defineConfig } = require('cypress');
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4173',
+    baseUrl: 'http://localhost:5173',
     specPattern: 'cypress/e2e/**/*.cy.{ts,tsx}',
     supportFile: 'cypress/support/e2e.ts',
   },
